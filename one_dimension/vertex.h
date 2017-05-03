665eb4cc0ed60c5188fff65c4e96dba427bddf7d
Ben Morton <morton@roe.ac.uk>
2017-05-03 14:02:21 +0100

    added new vertex class

---
+++ one_dimension/vertex.h
@@ -0,0 +1,97 @@
+class vertex{
+
+private:
+
+	float x;
+	float mass,momentum,energy;
+	float density,velocity,pressure;
+	float u_variables[3],fluxes[3];
+	float residual;
+
+public:
+
+
+//setter functions preventing varaibles being changes accidentally
+	void set_x(float new_x){
+		x = new_x;
+	}
+
+	void set_mass(float new_mass){
+		mass = new_mass;
+	}
+
+	void set_momentum(float new_momentum){
+		momentum = new_momentum;
+	}
+
+	void set_energy(float new_energy){
+		energy = new_energy;
+	}
+
+	void set_density(float new_density){
+		density = new_density;
+	}
+
+	void set_velocity(float new_velocity){
+		velocity = new_velocity;
+	}
+
+	void set_pressure(float new_pressure){
+		pressure = new_pressure;
+	}
+
+
+	//getter functions for extracting values of variables
+	float get_x(){
+		return x;
+	}
+
+	float get_mass(){
+		return mass;
+	}
+
+	float get_momentum(){
+		return momentum;
+	}
+
+	float get_energy(){
+		return energy;
+	}
+
+	float get_density(){
+		return density;
+	}
+
+	float get_velocity(){
+		return velocity;
+	}
+
+	float get_pressure(){
+		return pressure;
+	}
+
+	float get_u0(){
+		return u_variables[0];
+	}
+
+	float get_u1(){
+		return u_variables[1];
+	}
+
+	float get_u2(){
+		return u_variables[2];
+	}
+
+	float get_f0(){
+		return fluxes[0];
+	}
+
+	float get_f1(){
+		return fluxes[1];
+	}
+
+	float get_f2(){
+		return fluxes[2];
+	}
+
+};

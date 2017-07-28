corrected flux average calculation

one_dimension/roe/density_plot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #x,density = np.loadtxt("density.txt", usecols = (0,1))
 
-n_points = 50
+n_points = 200
 dx = 20.0/float(n_points)
 
 data = np.loadtxt("density.txt")                       # myfile.txt contains 4 columns of numbers
@@ -11,6 +11,8 @@ x0,rho0 = data[0:n_points-1,0]+0.5*dx, data[0:n_points-1,1]
 x1,rho1 = data[n_points:2*n_points-1,0]+0.5*dx, data[n_points:2*n_points-1,1]
 x2,rho2 = data[2*n_points:3*n_points-1,0]+0.5*dx, data[2*n_points:3*n_points-1,1]
 x3,rho3 = data[3*n_points:4*n_points-1,0]+0.5*dx, data[3*n_points:4*n_points-1,1]
+x4,rho4 = data[4*n_points:5*n_points-1,0]+0.5*dx, data[4*n_points:5*n_points-1,1]
+x5,rho5 = data[5*n_points:6*n_points-1,0]+0.5*dx, data[5*n_points:6*n_points-1,1]
 
 diff1 = rho1 - rho0
 
@@ -32,6 +34,8 @@ plt.plot(x0,rho0)
 plt.plot(x1,rho1)
 plt.plot(x2,rho2)
 plt.plot(x3,rho3)
+plt.plot(x4,rho4)
+plt.plot(x5,rho5)
 
 #plt.plot(x0,diff1)
 
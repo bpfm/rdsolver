098e38480caef5119b9f5e108b252a764af76710
Ben Morton <morton@cuillin.roe.ac.uk>
2019-12-16 13:46:18 +0000

    added run command for Cuillin

--- rd/run.sh
+++ rd/run.sh
@@ -1,9 +1,10 @@
 #g++ main.cpp -I /usr/local/opt/openblas/include /usr/local/opt/openblas/lib/libopenblas.a
 #./a.out
 
-clang++ -Xpreprocessor -fopenmp -L /usr/local/opt/llvm/lib -lomp -I /usr/local/opt/openblas/include /usr/local/opt/openblas/lib/libopenblas.a main.cpp -o lairds
-./lairds
+#clang++ -Xpreprocessor -fopenmp -L /usr/local/opt/llvm/lib -lomp -I /usr/local/opt/openblas/include /usr/local/opt/openblas/lib/libopenblas.a main.cpp -o lairds
+#./lairds
 
+clang++ -I /home/morton/local/include  -L /home/morton/local/lib -lopenblas -o lairds main.cpp
 
 # cd output
 # pythonw plot1D.py &

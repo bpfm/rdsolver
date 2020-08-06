37f8124a4e73f9c64461c986105eab45d5530e72
Ben Morton <morton@roe.ac.uk>
2020-08-06 17:47:15 +0100

    restored exit on undefined inversion

--- rd/inverse.cpp
+++ rd/inverse.cpp
@@ -14,6 +14,7 @@ lapack_int mat_inv(double *A, unsigned n, double X, double Y, int step){
                         std::cout << A[i] << "\t";
                         if((i+1)%n == 0){std::cout << std::endl;}
                 }
+		exit(0);
                 return ret;
         }
         ret = LAPACKE_dgetri(LAPACK_COL_MAJOR,n,A,n,ipiv);

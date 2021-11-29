87cf4ba81f14d247d8ecbfca0e22d0b2758afc21
Ben Morton <morton@roe.ac.uk>
2021-11-29 10:27:33 +0000

    included MacBook CGAl compile example

--- rd/triangulation/cgal/2d/compile_cgal_2d.sh
+++ rd/triangulation/cgal/2d/compile_cgal_2d.sh
@@ -1,3 +1,4 @@
+#PATH_TO_CGAL_SCRIPTS="/Users/benmorton/CGAL-5.0.2/scripts/cgal_create_CMakeLists"
 PATH_TO_CGAL_SCRIPTS="/home/morton/CGAL-5.0.2/Scripts/scripts/cgal_create_CMakeLists"
 bash $PATH_TO_CGAL_SCRIPTS -s cgal_periodic2D
 cmake -DCMAKE_BUILD_TYPE=Release .

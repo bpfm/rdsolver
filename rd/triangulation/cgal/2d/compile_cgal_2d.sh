a2cf94b57ade8c39392cc5b957f5738eddc6a354
Ben Morton <morton@roe.ac.uk>
2021-11-29 09:45:41 +0000

    generalised cgal compile script

--- rd/triangulation/cgal/2d/compile_cgal_2d.sh
+++ rd/triangulation/cgal/2d/compile_cgal_2d.sh
@@ -1,3 +1,3 @@
-PATH_TO_CGAL_SCRIPTS="/home/morton/cgal-releases-CGAL-5.0.2/Scripts/scripts/cgal_create_CMakeLists"
+PATH_TO_CGAL_SCRIPTS="/home/morton/CGAL-5.0.2/Scripts/scripts/cgal_create_CMakeLists"
 bash $PATH_TO_CGAL_SCRIPTS -s cgal_periodic2D
 cmake -DCMAKE_BUILD_TYPE=Release .
--- rd/triangulation/cgal/3d/compile_cgal_3d.sh
+++ rd/triangulation/cgal/3d/compile_cgal_3d.sh
@@ -1,2 +1,3 @@
-bash ~/CGAL-5.0.2/scripts/cgal_create_CMakeLists -s cgal_periodic3D
+PATH_TO_CGAL_SCRIPTS="/home/morton/CGAL-5.0.2/Scripts/scripts/cgal_create_CMakeLists"
+bash $PATH_TO_CGAL_SCRIPTS -s cgal_periodic3D
 cmake -DCMAKE_BUILD_TYPE=Release .

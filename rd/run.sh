b73e83d938af8d943b9af7be75572c7b4ff6e8ab
Ben Morton <morton@roe.ac.uk>
2024-10-22 14:28:45 +0100

    normal compile time setting changes

--- rd/run.sh
+++ rd/run.sh
@@ -4,5 +4,5 @@
 # clang++ -Xpreprocessor -fopenmp -L /usr/local/opt/llvm/lib -lomp -I /usr/local/opt/openblas/include /usr/local/opt/openblas/lib/libopenblas.a main.cpp -o lairds
 # ./lairds
 
-clang++ -I /home/morton/local/include  -L /home/morton/local/lib -lopenblas -o lairds main.cpp
+# clang++ -I /home/morton/local/include  -L /home/morton/local/lib -lopenblas -o lairds main.cpp
 # ./lairds
